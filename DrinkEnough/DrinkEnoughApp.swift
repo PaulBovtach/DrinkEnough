d1e30258c5f6580836a01fe52cfdc67385eea0eb
Paul Bovtach <natanbov@gmai.com>
2025-12-18 20:19:53 +0200

    Initial Commit

---
+++ DrinkEnough/DrinkEnoughApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkEnoughApp.swift
+//  DrinkEnough
+//
+//  Created by Paul Bovtach on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DrinkEnoughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
