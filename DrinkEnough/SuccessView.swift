ae0badd3f0ba9d9401d195e3a9c8b434d4552f25
Paul Bovtach <natanbov@gmai.com>
2025-12-19 11:08:12 +0200

    added SuccessView if user finished his daily plan

---
+++ DrinkEnough/SuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessView.swift
+//  DrinkEnough
+//
+//  Created by Paul Bovtach on 19.12.2025.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessView()
+}
